e158f677c39046630b45e73a962f1c5b19f060f7
Aditya Gupta <ag7998703@gmail.com>
2025-09-26 22:43:16 +0530

    4th

--- server.js
+++ server.js
@@ -9,22 +9,10 @@ const PORT = 4000;
 
 // connectDB();
 
-
-const allowedOrigins = ["http://localhost:4000", "https://zuris-frontend.vercel.app"];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
 
 
 
